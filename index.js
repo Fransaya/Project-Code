8f11df1fee47bcd8e01953bbc363f57fe04c1ae7
Francisco Sayago <fransayago04@gmail.com>
2023-09-27 12:03:58 -0300

    Intento de envio de email input a negocio para muestreo en input, falta terminar, fran 27/09 12:03

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const botonLogin = document.getElementById("botonLogin")
 
 const emails = ["admin@compu-tech.com", "ventas@compu-tech.com", "tecnicos@compu-tech.com"]
 const passwords = ["admin123", "elMasBonitoDelBarrio", "jKL743_"]
-
+let emailEnvio=""
 botonLogin.addEventListener("click", () => {
     if (emails.indexOf(email.value) === passwords.indexOf(password.value)) {
         window.location.href = "negocio.html";
-        
+        console.log(`valor de email console index${emails.indexOf(email.value)}`);
+        emailEnvio=email;
     };
 });
-
+/// FALTA VER CON COMO OBTENER EMAIL INGRESADO POR USUARIO, PARA DESPUES ENVIARLOS O PASARLO A PAGINA DE NOGICO
+// EN CONSOLA NO ME MUESTRA NUNGUN VALOR DE EMAIL.
+console.log(`valor de email a enviar: ${emailEnvio}`)
+localStorage.setItem("email",emailEnvio);
 
